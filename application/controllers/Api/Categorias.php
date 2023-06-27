d2a6abd5eb1ba135846ba64725a5b0938bf877cd
KitziaAN <utp0144170@alumno.utpuebla.edu.mx>
2023-06-27 10:23:09 -0600

    CRUD de API REST

---
+++ application/controllers/Api/Categorias.php
@@ -0,0 +1,46 @@
+<?php
+
+require APPPATH . 'libraries/REST_Controller.php';
+
+class Categorias extends REST_Controller{
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
+
+    //Obtener
+    public function index_get($id=0) {
+        //En caso de recuperar una categoría especifica
+        if (!empty($id)){
+            $data = $this->db->get_where('categorias', ['id_categorias'=>$id])->row_array();
+        }
+        else{
+            $data = $this->db->get("categorias")->result();
+        }
+        $this->response($data, REST_Controller::HTTP_OK);
+        
+        
+    }
+
+    //Insertar
+    public function index_post(){
+        $input = $this->input->post();
+        $this->db->insert("categorias", $input);
+        $this->response(['Categoria agregada'], REST_Controller::HTTP_OK);
+    }
+
+    //Actualizar
+    public function index_put($id){
+        $input = $this->put();
+        $this->db->update("categorias", $input, array("id_categorias" => $id));
+        $this->response(["Categoria actualizado"], REST_Controller::HTTP_OK);
+    }
+
+    //Eliminar
+    public function index_delete($id){
+        $this->db->delete("categorias", array("id_categorias" => $id));
+        $this->response("Categoria eliminada", REST_Controller::HTTP_OK);
+        
+    }
+
+}
